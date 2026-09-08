26bb18549cac6a017605ebbd397d5aa77d9c4aff
ci <ops@joinself.com>
2026-09-08 15:22:39 +0100

    release: 0.1.0-rc.17

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ZktfSDK",
-            url: "https://download.joinself.com/zktf-sdk-ios/ZktfSDK-0.1.0-rc.16.xcframework.zip",
-            checksum: "34fd70bc641abe3c7b28542be1c57534672a95c1f44d131f8ea61e308f91e358"
+            url: "https://download.joinself.com/zktf-sdk-ios/ZktfSDK-0.1.0-rc.17.xcframework.zip",
+            checksum: "70ef952062cd74cd37e80d511d5aabd80bc94a282c48b944c8494f9459b35ef1"
         ),
     ]
 )
